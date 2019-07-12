ab8006ef9afbcc9eceaf545862eec982d069da11
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-07-12 10:10:11 -0500

    created elements in header js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,10 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+  // create elements
+  const header = document.createElement("header");
+  const date = document.createElement("span");
+  const title = document.createElement("h1");
+  const temp = document.createElement("span");
+}
